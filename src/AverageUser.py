a27be1824458e8fac52a8c2d28b2b4f6146e24a2
pablodll <pablo.daurell@gmail.com>
2022-04-21 00:07:34 +0200

    Errores de printear explicacion arreglados

--- src/AverageUser.py
+++ src/AverageUser.py
@@ -4,10 +4,11 @@ import collections
 
 class AverageUser:
     
-    def __init__(self, data, artworks_info, atributes_list):
+    def __init__(self, data, artworks_info, atributes_users, atributes_artworks):
         self.data = data
         self.artworks_info = artworks_info
-        self.atributes_list = atributes_list
+        self.atributes_users = atributes_users
+        self.atributes_artworks = atributes_artworks
         self.stats_dicts = {}
     
     def computeAverageUser(self, n_artworks=3):
@@ -25,22 +26,18 @@ class AverageUser:
                 if atr not in ['userId', 'cluster', 'positive', 'negative', 'mixed']:
                     mode = c[atr].mode()
                     perc = np.multiply(np.divide(c[atr].value_counts().to_list(), len(c)), 100)
-                    
+
                     averageUser[atr] = mode[0]
-                    
+
                     aux_dict[atr] = list(zip(mode, perc))
                     
-                       
-            for atr in c.columns:
-                if atr in ['positive', 'negative', 'mixed']:
-                    artworks = collections.Counter([artw for usr in c[atr] for artw in usr])
-            ##Artista mas gustado por cluster
-                    if(atr == "positive"):
-                        posC = collections.Counter([item for sublist in [self.artworks_info[self.artworks_info['ID'] == artw]["Artist"].to_list() for artw in artworks] for item in sublist])
-                        aux_dict["Most liked artist"] = posC.most_common(1)
-                    averageUser[atr] = [item[0] for item in artworks.most_common(n_artworks)]
+            for atr in ['positive', 'negative', 'mixed']:
+                # for atr in self.atributes_artworks:
+                artworks = collections.Counter([artw for usr in c[atr] for artw in usr])
+                averageUser[atr] = [item[0] for item in artworks.most_common(n_artworks)]
 
-            self.stats_dicts[c.cluster.iloc[0]] = aux_dict 
+            clusterId = c.cluster.iloc[0]
+            self.stats_dicts[clusterId] = aux_dict
             averageUser['cluster'] = c.cluster.iloc[0]
             newUsers.append(averageUser)
             newId += 1
@@ -48,15 +45,22 @@ class AverageUser:
         self.users_df = pd.DataFrame(newUsers)
         return self.users_df
     
+    def mostLikedFromCategory(self, atr):
+        ## Artista mas gustado por cluster
+        category = collections.Counter([item for sublist in [self.artworks_info[self.artworks_info['ID'] == artw][atr].to_list() for artw in artworks] for item in sublist])
+        if len(artists.most_common(1)) > 0:
+            aux_dict["Most " + atr] = [(category.most_common(1)[0][0], artists.most_common(1)[0][1] / len(c) * 100)]
+    
     def printExplanation(self):
         if len(self.stats_dicts) != 0:
             for cluster, d in self.stats_dicts.items():
                 print('------------------------')
                 print("Cluster: ", cluster)
+                print("\tIndividuos: ", len(self.data[self.data.cluster == cluster]))
                 for atr, perc in d.items():
-                    print(atr,":")
+                    print("\t" + atr,":")
                     for x in perc:
-                        print(x[0],x[1],"%")
+                        print("\t\t",  x[0], "(", x[1], "%)")
                 
                 for polarity in ['positive', 'negative', 'mixed']:
                     print("\t--Top",self.n_artworks, " " + polarity + "--")
@@ -64,7 +68,8 @@ class AverageUser:
                         print("0 artworks found with", polarity, " polarity")
                     for artworks in self.users_df[self.users_df['cluster'] == cluster][polarity]:
                         for art in artworks:
+                            print("\t\t\tTitle:" + self.artworks_info[self.artworks_info['ID'] == art]['Title'].to_list()[0])
                             for col in self.artworks_info.columns:
-                                if col in self.atributes_list:
-                                    print(col + ": " + self.artworks_info[self.artworks_info['ID'] == art][col].to_list()[0])
+                                if col in self.atributes_artworks:
+                                    print("\t\t\t" + col + ": " + self.artworks_info[self.artworks_info['ID'] == art][col].to_list()[0])
                     
