31c9e4517564dad448ba1e26d755e1b268ba8b11
AlbertoGarciaDomenech <alberg22@ucm.es>
2022-04-20 13:34:52 +0200

    Calculo artista mas gustado por cluster

--- src/AverageUser.py
+++ src/AverageUser.py
@@ -11,6 +11,7 @@ class AverageUser:
         self.stats_dicts = {}
     
     def computeAverageUser(self, n_artworks=3):
+        self.n_artworks = n_artworks
         clusters = []
         for c in self.data.cluster.unique():
             clusters.append(self.data[self.data['cluster'] == c])
@@ -29,13 +30,17 @@ class AverageUser:
                     
                     aux_dict[atr] = list(zip(mode, perc))
                     
-            self.stats_dicts[c.cluster.iloc[0]] = aux_dict
-                    
+                       
             for atr in c.columns:
                 if atr in ['positive', 'negative', 'mixed']:
                     artworks = collections.Counter([artw for usr in c[atr] for artw in usr])
+            ##Artista mas gustado por cluster
+                    if(atr == "positive"):
+                        posC = collections.Counter([item for sublist in [self.artworks_info[self.artworks_info['ID'] == artw]["Artist"].to_list() for artw in artworks] for item in sublist])
+                        aux_dict["Most liked artist"] = posC.most_common(1)
                     averageUser[atr] = [item[0] for item in artworks.most_common(n_artworks)]
-                        
+
+            self.stats_dicts[c.cluster.iloc[0]] = aux_dict 
             averageUser['cluster'] = c.cluster.iloc[0]
             newUsers.append(averageUser)
             newId += 1
@@ -49,10 +54,14 @@ class AverageUser:
                 print('------------------------')
                 print("Cluster: ", cluster)
                 for atr, perc in d.items():
-                    print(atr, perc)
+                    print(atr,":")
+                    for x in perc:
+                        print(x[0],x[1],"%")
                 
                 for polarity in ['positive', 'negative', 'mixed']:
-                    print("\t" + polarity)
+                    print("\t--Top",self.n_artworks, " " + polarity + "--")
+                    if(len(self.users_df[self.users_df['cluster'] == cluster][polarity]) == 0):
+                        print("0 artworks found with", polarity, " polarity")
                     for artworks in self.users_df[self.users_df['cluster'] == cluster][polarity]:
                         for art in artworks:
                             for col in self.artworks_info.columns:
