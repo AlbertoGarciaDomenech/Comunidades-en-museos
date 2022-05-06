55474b34486e02c76ddad477f0c9c5ef4ed13f44
pablodll <pablo.daurell@gmail.com>
2022-05-06 14:35:31 +0200

    Primera parte Interfaz de usuario unificada terminada

--- src/ArtworksMatrix.py
+++ src/ArtworksMatrix.py
@@ -3,11 +3,11 @@ import numpy as np
 from src.SimilarityArtworks import SimilarityArtworks
 
 class ArtworksMatrix():
-    def __init__(self, data_art, function_dict, weight_dict, colors_path=None):
+    def __init__(self, data_art, function_dict, weight_dict):#, colors_path=None):
         self.data_art = data_art
         self.function_dict = function_dict
         self.weight_dict = weight_dict
-        self.colors_path = colors_path
+        # self.colors_path = colors_path
         self.simArtworks = SimilarityArtworks()
         
     def getSimilarityMatrix(self):
@@ -17,7 +17,7 @@ class ArtworksMatrix():
             j = 0
             for b in self.data_art['ID']:
                 artworks_matrix[i][j] = self.computeSimilarity(a, b)
-                j +=1
+                j+=1
             i+=1
             
         return pd.DataFrame(artworks_matrix, index = [i for i in self.data_art['ID']], columns = [i for i in self.data_art['ID']])
@@ -27,11 +27,15 @@ class ArtworksMatrix():
         total_sim = 0
         for col, fun in self.function_dict.items():
             if fun is not None:
-                result = getattr(self.simArtworks, fun)(self.data_art).computeSimilarity(A, B)
+                if col not in self.data_art.columns: # Atributo con fichero externo
+                    result = getattr(self.simArtworks, fun)(self.data_art, col).computeSimilarity(A, B)
+                else:
+                    result = getattr(self.simArtworks, fun)(self.data_art).computeSimilarity(A, B)
+                    
                 total_sim += result * self.weight_dict.get(col)
         
-        if self.colors_path is not None:
-            total_sim += self.simArtworks.SimilarityColors(self.data_art, self.colors_path).computeSimilarity(A, B) * self.weight_dict.get("Colors")
+        # if self.colors_path is not None:
+        #     total_sim += self.simArtworks.SimilarityColors(self.data_art, self.colors_path).computeSimilarity(A, B) * self.weight_dict.get("Colors")
         
         
         return total_sim
